49191a66de29138f6798e610de18c07f3517e4b4
juarezlopesdev <juarezlopes599@gmail.com>
2025-06-26 21:05:40 -0300

    Corrigindo o Main

--- json-processor/app/Main.hs
+++ json-processor/app/Main.hs
@@ -8,6 +8,19 @@ import Options.Applicative (execParser, info, helper, fullDesc, progDesc, (<**>)
 import qualified Data.ByteString.Lazy as BL
 import Data.Aeson (Value, decode)
 import System.Exit (exitFailure, exitSuccess)
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import Data.Text (Text)
+import qualified Data.ByteString as B
+
+-- Função para remover BOM e converter para UTF-8
+ensureUtf8 :: BL.ByteString -> BL.ByteString
+ensureUtf8 bs
+  | B.take 3 (BL.toStrict bs) == B.pack [0xEF, 0xBB, 0xBF] =  -- UTF-8 BOM
+      BL.fromStrict $ B.drop 3 (BL.toStrict bs)
+  | B.take 2 (BL.toStrict bs) == B.pack [0xFF, 0xFE] ||      -- UTF-16 LE BOM
+    B.take 2 (BL.toStrict bs) == B.pack [0xFE, 0xFF] =       -- UTF-16 BE BOM
+      BL.fromStrict $ encodeUtf8 (decodeUtf8 (BL.toStrict bs))
+  | otherwise = bs
 
 main :: IO ()
 main = do
@@ -17,7 +30,7 @@ main = do
     
   case cmd of
     Filter expr input output -> do
-      inputData <- BL.readFile input
+      inputData <- ensureUtf8 <$> BL.readFile input
       case filterJson expr inputData of
         Left err -> do
           putStrLn $ "Erro na filtragem: " ++ err
@@ -31,8 +44,8 @@ main = do
           exitSuccess
           
     ValidateSchema schema input -> do
-      schemaData <- BL.readFile schema
-      inputData  <- BL.readFile input
+      schemaData <- ensureUtf8 <$> BL.readFile schema
+      inputData  <- ensureUtf8 <$> BL.readFile input
       case validateJson schemaData inputData of
         Left err -> do
           putStrLn $ "Erro na validação: " ++ err
